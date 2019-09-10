c0d9f9c8a3e38c8531da81e7beb660a1ba6a81c8
allinvestglobal <53827177+allinvestglobal@users.noreply.github.com>
2019-09-10 19:51:51 +0200

    Add files via upload

---
+++ job1.py
@@ -0,0 +1,356 @@
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '01.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '02.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '03.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '04.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '05.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '06.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '07.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '08.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '09.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '10.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '11.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '12.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '13.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '14.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '15.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+import time
+
+time.sleep(600)  # delays for 10 minutes
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '16.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '17.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '18.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '19.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '20.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '21.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '22.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '23.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '24.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '25.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '26.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '27.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '28.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '29.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'espanaempleoa'
+password = '20million'
+image = '30.jpeg'
+text = 'Reunimos a empleadores y empleados y también tenemos un equipo interno listo para trabajar en miles de trabajos desde 10 euro'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
